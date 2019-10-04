bf16121e89f81325b115952d03333cc45413bee9
Thiago <thiago__rocha17@hotmail.com>
2019-10-04 15:13:59 -0300

    adicionei o js no principal e 2 operandos no trabalho de leopoldo

--- JavaScript-VSCode/Trabalho Leopoldo/calculadora.js
+++ JavaScript-VSCode/Trabalho Leopoldo/calculadora.js
@@ -1,8 +1,8 @@
 function calcularPrincipal(){
 
-    var resultado = document.getElementById("resultado");
-    var operacao =  document.getElementsByName("operacao");
-    var x = Number(document.getElementById("valor").value);
+    let resultado = document.getElementById("resultado");
+    let operacao =  document.getElementsByName("operacao");
+    let x = Number(document.getElementById("valor").value);
 
     for(let i = 0; i < operacao.length; i++){
         if(operacao[i].checked){
@@ -37,4 +37,40 @@ function calcularPrincipal(){
     }else{
         alert("Voce precisa selecionar uma função!");
     }
+}
+
+function limpaPrincipal(){
+    let valor = document.getElementById("valor");
+    let resultado = document.getElementById("resultado");
+    valor.value = "";
+    resultado.value = "";
+}
+
+function calcularDoisOperandos(){
+    let n1 = Number(document.getElementById("valor1").value);
+    let n2 = Number(document.getElementById("valor2").value);
+    let resultado = document.getElementById("resultado");
+    let operador =  document.getElementById("operador").value;
+
+    if(operador == "mais"){
+        resultado.value = n1 + n2;
+    }else if(operador == "menos"){
+        resultado.value = n1 - n2;
+    }else if(operador == "vezes"){
+        resultado.value = n1 * n2;
+    }else if(operador == "divisao"){
+        resultado.value = n1 / n2;
+    }else if(operador == "mod"){
+        resultado.value = n1 % n2;
+    }
+
+}
+
+function limpaDoisOperandos(){
+    let n1 = document.getElementById("valor1");
+    let n2 = document.getElementById("valor2");
+    let resultado = document.getElementById("resultado");
+    n1.value = "";
+    n2.value = "";
+    resultado.value = "";
 }
